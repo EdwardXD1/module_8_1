ce9c0af1820f3f641c8a312e3d696bb34984404a
EdwardXD1 <edwardkulagin@mail.ru>
2024-08-22 14:46:01 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,12 @@
+def add_everything_up(a, b):
+    try:
+        print(a + b)
+    except TypeError:
+        a = str(a)
+        b = str(b)
+        c = a + b
+        return c
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
